2d969f2d67d30ec5cd42f5eda97790e28fdd459f
rodonisi <simon@rodoni.ch>
2023-12-29 03:38:30 +0100

    headers

--- libtesseract.h
+++ libtesseract.h
@@ -18,4 +18,6 @@ FOUNDATION_EXPORT const unsigned char libtesseractVersionString[];
 // In this header, you should import all the public headers of your framework
 // using statements like #import <SwiftyTesseract/PublicHeader.h>
 
+#import <libtesseract/array_internal.h>
+#import <libtesseract/baseapi.h>
 #import <libtesseract/pix_internal.h>
