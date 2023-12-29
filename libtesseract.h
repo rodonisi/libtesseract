4b1dbd3afee82e96220682113131c3e4ef45a1c7
rodonisi <simon@rodoni.ch>
2023-12-29 02:40:43 +0100

    only pix

--- libtesseract.h
+++ libtesseract.h
@@ -8,8 +8,6 @@
 #import <Foundation/Foundation.h>
 #import <libtesseract/allheaders.h>
 #import <libtesseract/capi.h>
-#import <libtesseract/pix_internal.h>
-#import <libtesseract/ccbord_internal.h>
 
 //! Project version number for SwiftyTesseract.
 FOUNDATION_EXPORT double libtesseractVersionNumber;
@@ -17,4 +15,7 @@ FOUNDATION_EXPORT double libtesseractVersionNumber;
 //! Project version string for SwiftyTesseract.
 FOUNDATION_EXPORT const unsigned char libtesseractVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTesseract/PublicHeader.h>
+// In this header, you should import all the public headers of your framework
+// using statements like #import <SwiftyTesseract/PublicHeader.h>
+
+#import <libtesseract/pix_internal.h>
